2921545236730985fb741d94306ba29b31c49c88
Minh To <minhqto3@gmail.com>
2020-02-08 02:17:35 -0500

    had to reinstall and reconfig dart and flutter

--- simon_sene_carl/lib/main.dart
+++ simon_sene_carl/lib/main.dart
@@ -1,32 +1,45 @@
 import 'package:flutter/material.dart';
 import 'test.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
 
 void main(){
   //MaterialApp() is a widget that is like a wrapper for the rest of the widgets in our app
   runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.pinkAccent,
-        title: Text("Hello Wrldsss!"),
-        titleSpacing: 2.0,
-        centerTitle: true,
-      ),
-      backgroundColor: Colors.pinkAccent,
-      body: Center(
-        child: Text("My name is Jeff",
-        style: TextStyle(
-          fontSize: 30,
-          fontFamily: 'MonarchDisplay'
-        ),),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {}, //anonymous function
-        child: Center(
-          child: Text("lick me!"),
-        ),
-    ),
-    ),
-
+    home: GHFlutter()
 
   ));
+}
+
+
+class GHFlutter extends StatefulWidget{
+  @override
+  createState() =>GHFlutterState();
+}
+
+class GHFlutterState extends State<GHFlutter>{
+  var _members = []; //empty array
+  final _biggerfont = const TextStyle(fontSize: 18);
+
+  _loadData() async {
+    String dataURL = "https://api.github.com/orgs/raywenderlich/members";
+    http.Response response = await http.get(dataURL);
+    setState(() {
+      _members = json.decode(response.body);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        toolbarOpacity: 0.5,
+        title: Text(Strings.appTitle),
+
+      )
+    );
+  }
 }
