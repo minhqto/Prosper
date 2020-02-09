7e8cf15778fadd6867b8f9051c8b1185927ad411
Minh To <minhqto3@gmail.com>
2020-02-09 02:18:04 -0500

    finished the article page

--- simon_sene_carl/lib/wall.dart
+++ simon_sene_carl/lib/wall.dart
@@ -1,13 +1,12 @@
 
 import 'dart:ffi';
-
 import 'package:simon_sene_carl/themeColor.dart';
-
 import 'fullscreen_image.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'dart:async';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+
 
 const String testDevice = '';
 
@@ -93,7 +92,48 @@ class _WallScreenState extends State<WallScreen> {
               TextStyle(fontFamily: "MonarchDisplay", color: Colors.black)),
         ),
         drawer: Drawer(),
-      body: _buildBody(context)
+      body: Column(
+        children: <Widget>[
+          Text("If you want to learn more...",
+          style: TextStyle(
+            fontFamily: "Medium",
+            fontSize: 20
+          )),
+          Container(
+              padding: EdgeInsets.only(bottom: 15),
+              child: Text("We have selected several articles for you if you are interested in exploring more about Trading Stocks",
+              style: TextStyle(
+                  fontFamily: "Regular",
+                  fontSize: 12
+              ))),
+          SizedBox(
+            height: 570,
+            child: _buildBody(context)
+          ),
+          Container(
+            child: OutlineButton(
+              onPressed: (){Navigator.push(context, MaterialPageRoute(builder: (context) => SearchStock()))},
+              child: Text("Practice Buying a Stock",
+                  style: TextStyle(
+                      fontFamily: "Regular",
+                      fontSize: 16
+                  )
+              )
+            )
+          ),
+          Container(
+            child: FlatButton(
+              child: Text("Go to Dashboard",
+                  style: TextStyle(
+                  fontFamily: "Regular",
+                  fontSize: 14
+              ))
+            )
+          )
+
+        ],
+      ),
+
     );
   }
 }
