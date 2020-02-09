06ed28261facc9db034c1a7795b6e9b9f8704505
viet-quocnguyen <viet.nguyen@ceridian.com>
2020-02-08 19:15:40 -0500

    Second screen finished

--- simon_sene_carl/lib/SecondScreen.dart
+++ simon_sene_carl/lib/SecondScreen.dart
@@ -1,50 +1,28 @@
 import 'package:flutter/material.dart';
-import 'package:simon_sene_carl/ThirdScreen.dart';
-import "package:flappy_search_bar/flappy_search_bar.dart";
-import 'themeColor.dart';
+import 'package:simon_sene_carl/widgets/searchBar.dart';
 
 class SecondScreen extends StatelessWidget {
   @override
-  // Widget build(BuildContext context) {
-  //   return Scaffold(
-  //     appBar: AppBar(
-  //       title: Text('Second Screen'),
-  //     ),
-  //     body: Center(
-  //       child: RaisedButton(
-  //         child: Text('Go to the next screen'),
-  //         onPressed: () {
-  //           // Navigate to second route when tapped.
-  //           Navigator.push(
-  //             context,
-  //             MaterialPageRoute(builder: (context) => ThirdScreen()),
-  //           );
-  //         },
-  //       ),
-  //     ),
-  //   );
-  // }
-
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
 
     return Scaffold(
-      backgroundColor: getTheme(),
+      backgroundColor: const Color.fromRGBO(245, 246, 240, 1.0),
       body: Container(
-          height: size.height,
           width: size.width,
           padding: EdgeInsets.only(left: 20, right: 20, top: 48),
-          child: Column(
+          child: ListView(
             children: <Widget>[
               Container(
-                padding: const EdgeInsets.fromLTRB(0,20,0,20),
+                width: size.width,
+                height: size.height * 0.2,
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: <Widget>[
                     Text("Get Started",
                         style: TextStyle(
-                          fontFamily: "Regular",
+                          fontFamily: "Maax",
                           fontSize: 19,
                           color: Colors.grey,
                         )),
@@ -57,23 +35,8 @@ class SecondScreen extends StatelessWidget {
                   ],
                 ),
               ),
-
-              RaisedButton(
-                child: Text('Go to the next screen'),
-                onPressed: () {
-                  // Navigate to second route when tapped.
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => ThirdScreen()),
-                  );
-                },
-              ),
-              RaisedButton(
-                child: Text('Go back'),
-                onPressed: () {
-                  // Navigate to second route when tapped.
-                  Navigator.pop(context);
-                },
+              Container(
+                child: MySearchBar(),
               ),
             ],
           )),
